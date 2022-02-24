c605acc7f5499b1bf444abc7333fa1f0863b6dc6
Keith Stevens <fozziethebeat@gmail.com>
2022-02-24 13:14:21 +0900

    Addig a forgotten file

---
+++ nougyou-arigatou/react-native/final-version/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: 'rgb(76, 168, 186)',
+    background: 'rgb(255, 255, 255)',
+    card: 'rgb(122, 168, 76)',
+    text: 'rgb(255, 255, 255)',
+    primaryText: '#000',
+    border: 'rgb(76, 186, 94)',
+  },
+};
